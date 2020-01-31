648e9faa60cdf05d561971ec04b5422b908a353e
Pharoouzy <pharoouzy@yahoo.com>
2020-01-31 16:34:06 +0100

    Update Administrator.php

--- classes/Administrator.php
+++ classes/Administrator.php
@@ -1,8 +1,8 @@
 <?php
 
 	/**
-	 * If It's going to need the database, then it's 
-	 *probably smart to require it before we start.
+	 * Administrator Class
+	 *
 	 */
 
 	require_once(CLS_PATH.'Database.php');
